5dea47f90b9ef0606ab69aefc1c0298fffd96b05
GlamDaria <daryalutkova@yandex.ru>
2020-06-26 19:17:18 +0300

    Remove Mocks to Return API Data

--- src/panels/FitNews.js
+++ src/panels/FitNews.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import moment from 'moment';
-import { Panel, PanelHeader, Group, Banner, Header, Avatar, Button, Link } from '@vkontakte/vkui';
-// import axios from 'axios';
-import {mock} from './mock.js';
+import { Panel, PanelHeader, Group, Banner, Header, Avatar, Link } from '@vkontakte/vkui';
+import axios from 'axios';
 
 export default class FitNews extends React.Component {
   state = {
     banners: []
-  }
+  };
+
   async componentDidMount() {
-    // const res = await axios.get('https://fit.mospolytech.ru/api/1.0/news/');
-    // console.log(res);
+    const res = await axios.get('https://onepix.dev/recoby?referer=https://fit.mospolytech.ru/feed&url=https://fit.mospolytech.ru/api/1.0/news/');
+
     this.setState({
-      banners: mock,
+      banners: res.data,
     })
   }
 
@@ -24,30 +24,6 @@ export default class FitNews extends React.Component {
         <Group
           header={<Header mode="secondary">Самое свежее от факультета информационных технологий!</Header>}
         >
-        <Banner
-          mode="image"
-          header="Хакатон"
-          subheader="Приглашаем к участию!"
-          background={
-            <div
-              style={{
-                backgroundColor: '#65c063',
-                backgroundImage: 'url(https://sun1-90.userapi.com/Kdlb30B4agBUdxYasvTohGTBTiCmq1u23iIGCQ/3ilwpdcO7mY.jpg)',
-                backgroundPosition: 'cover',
-                backgroundSize: '100%',
-                backgroundRepeat: 'no-repeat',
-              }}
-            />
-          }
-          actions={<Button mode="overlay_primary">Подробнее</Button>}
-        />
-         <Banner
-          before={<Avatar size={96} mode="image" src="https://sun1-16.userapi.com/4r3Kj2rCJ0TWdFV3_GdgFzjPqlVL7NEFlSXacg/F6H_sQDZkro.jpg" />}
-          header="Онлайн стажировка "
-          subheader="Внимание-внимание!"
-          asideMode="expand"
-          actions={<Button>Подробнее</Button>}
-        />
         {
           this.state.banners.map(banner => (
             <Link  key={banner.id} href={`https://vk.com/moscowpolytech?w=wall${banner.owner_id}_${banner.vk_id}`}>
